329b92cb3a8e168d7c5d67d7d380349fc074512c
lucas pilette <pilettelucas@gmail.com>
2022-01-16 19:57:16 +0100

    first commit cv

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(elem =>{
+    elem.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(elem.getAttribute('href')).scrollIntoView({
+            behavior:"smooth",
+            offsetTop: 50
+        });
+    });
+})
